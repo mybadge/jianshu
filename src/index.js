2a1ed2ead28caeec6aff7e03c97c23011513508c
mybadge <mybadge@163.com>
2019-01-31 15:45:54 +0800

    add  styled-components 管理样式

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-
+import './index.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
