73b98adf1a292090dfebbadadf6f225a95a9b393
mybadge <mybadge@163.com>
2019-02-17 10:48:54 +0800

    add nav

--- src/Common/Header/Header.js
+++ src/Common/Header/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { HeaderWrapper, Logo } from './style';
+import { HeaderWrapper, Logo, Nav } from './style';
 
 class Header extends Component {
     render() {
         return (
             <HeaderWrapper>
                 <Logo />
+                <Nav />
             </HeaderWrapper>
         )
     }
